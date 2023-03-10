2eae102c6854eb1c6cd50acbcf13083fb4b895b1
Anand <anandsinghtechie@gmail.com>
2023-03-10 16:22:43 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing",{
+    strings:["","Student","Competitive Programmer","Web Developer","Android Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true 
+})
